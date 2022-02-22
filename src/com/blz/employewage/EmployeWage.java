ac1d24f019632a93995b6f821c69643bfa10d4e0
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-22 11:46:25 +0530

    Added: part time functionality

--- src/com/blz/employewage/EmployeWage.java
+++ src/com/blz/employewage/EmployeWage.java
@@ -2,18 +2,23 @@ package com.blz.employewage;
 
 public class EmployeWage {
         public static final int FULL_TIME=1;
-    public static final int EMP_WAGE_PER_HR=20;
+        public static final int PART_TIME=2;
+        public static final int EMP_WAGE_PER_HR=20;
 
     public static void main(String[] args) {
         int empHr=0;
         int empWage=0;
 	    System.out.println("Welcome to Employee Wage Computation Program");
 
-            int Empcheck=(int)(Math.random()*10) % 2;
+            int Empcheck=(int)(Math.random()*10) % 3;
 
             if(Empcheck==FULL_TIME){
                 empHr=8;
-                System.out.println("Employee is present");
+                System.out.println("Employee is working Full Time");
+            }
+            else if(Empcheck==PART_TIME){
+                empHr=4;
+                System.out.println("Employee is working Part Time");
             }
             else{
                 System.out.println("Employee is Absent");
