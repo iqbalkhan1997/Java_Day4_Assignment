ab1e83f7e179cb5f48dd47b7134d2e3cc291ae2f
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-22 11:23:52 +0530

    Added: usecase1 functionality

--- src/com/blz/employewage/EmployeWage.java
+++ src/com/blz/employewage/EmployeWage.java
@@ -4,5 +4,14 @@ public class EmployeWage {
 
     public static void main(String[] args) {
 	    System.out.println("Welcome to Employee Wage Computation Program");
+            int Present=1;
+            int Empcheck=(int)(Math.random()*10) % 2;
+
+            if(Empcheck==Present){
+                System.out.println("Employee is present");
+            }
+            else{
+                System.out.println("Employee is Absent");
+            }
     }
 }
